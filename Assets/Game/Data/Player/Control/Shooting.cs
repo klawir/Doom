f8a907a647eb4d363f9069ac0cff82d63511e76e
klawir <klawir1@o2.pl>
2019-12-21 02:19:47 +0100

    Basic shooting

---
+++ Assets/Game/Data/Player/Control/Shooting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Bullet bullet;
+    public Transform rifleBarrel;
+    public Transform cameraTransform;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GameObject tempObj = Instantiate(bullet.gameObject, rifleBarrel.position, bullet.transform.rotation) as GameObject;
+            tempObj.GetComponent<Bullet>().Move(cameraTransform);
+        }
+    }
+}
---
+++ Assets/Game/Data/Weapons/AK-74U/ammo/bullet/Core/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float speed;
+
+    public void Move(Transform cameraTransform)
+    {
+        projectile.velocity = cameraTransform.forward * speed;
+    }
+}
